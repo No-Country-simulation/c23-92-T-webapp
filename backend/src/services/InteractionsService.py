36ca26cefb090227904528fc458421c319f7d271
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2025-01-21 17:42:23 -0500

    new services added

---
+++ backend/src/services/InteractionsService.py
@@ -0,0 +1,27 @@
+from src.repositories.InteractionsRepository import InteractionsRepository
+from src.services.JournalsService import JournalsService
+from src.models.Interactions import Interactions
+from src.utils.Logger import Logger
+
+class InteractionsService:
+    def __init__(self):
+        self.interactions_repository = InteractionsRepository()
+        self.journals_service = JournalsService()
+
+    def create_interaction(self, user_id, title, state, content, response):
+        try:
+            journal = self.journals_service.get_or_create_today_journal(user_id)
+            interaction = Interactions(title=title, state=state, content=content, response=response, journal_id=journal.id)
+            self.interactions_repository.add(interaction)
+            self.journals_service.increment_interactions_count(user_id=user_id, journal_id=journal.id)
+            return interaction
+        except Exception as ex:
+            Logger.add_to_log("error", f"Error al crear la interacción en InteractionsService: {ex}")
+            raise ex
+        
+    def list_interactions_by_journal(self, user_id, journal_id):
+        try:
+            return self.interactions_repository.get_by_journal_id(user_id, journal_id)
+        except Exception as ex:
+            Logger.add_to_log("error", f"Error al obtener las interacciones en InteractionsService: {ex}")
+            raise ex
---
+++ backend/src/services/UserService.py
@@ -0,0 +1,39 @@
+from src.repositories.UserRepository import UserRepository
+from src.utils.Logger import Logger
+import traceback
+import pytz
+
+class UserService:
+    def __init__(self):
+        self.user_repository = UserRepository()
+
+    def update_timezone(self, user_id: int, new_timezone: str):
+        try:
+            if new_timezone not in pytz.all_timezones:
+                return {
+                    'success': False,
+                    'error': 'Invalid timezone'
+                }
+            
+            user = self.user_repository.get_by_id(user_id)
+            if not user:
+                return {
+                    'success': False,
+                    'error': 'User not found'
+                }
+            
+            user.timezone = new_timezone
+
+            self.user_repository.update(user)
+
+            return {
+                'success': True,
+                'message': 'Timezone updated successfully'
+            }
+        except Exception as ex:
+            Logger.add_to_log("error", str(ex))
+            Logger.add_to_log("error", traceback.format_exc())
+            return {
+                'success': False,
+                'error': 'Internal server error'
+            }
