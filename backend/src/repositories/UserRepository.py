be6b066d5ebe2962a3bf8a7f37ae13f0112ba7d7
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2025-02-06 13:26:49 -0500

    get_username_by_id and get_email_by_id added

--- backend/src/repositories/UserRepository.py
+++ backend/src/repositories/UserRepository.py
@@ -36,18 +36,12 @@ class UserRepository:
         return user.timezone
 
     def update_username(self, user_id, username):
-        username_exists = self.get_user_by_username(username)
-        if username_exists:
-            return None
         user = self.get_by_id(user_id)
         user.username = username
         db.session.commit()
         return user
 
     def update_email(self, user_id, email):
-        email_exists = self.get_user_by_email(email)
-        if email_exists:
-            return None
         user = self.get_by_id(user_id)
         user.email = email
         db.session.commit()
@@ -58,3 +52,11 @@ class UserRepository:
         user.timezone = timezone
         db.session.commit()
         return user
+    
+    def get_username_by_id(self, user_id):
+        user = self.get_by_id(user_id)
+        return user.username
+    
+    def get_email_by_id(self, user_id):
+        user = self.get_by_id(user_id)
+        return user.email
